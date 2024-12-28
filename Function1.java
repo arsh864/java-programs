6b82d6899fcd2cebf7151447da2c85629844aaef
arsh864 <arsh283514@gmail.com>
2024-12-28 13:12:46 +0530

    update function1.java

--- Function1.java
+++ Function1.java
@@ -5,7 +5,6 @@ public class Function1 {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
         return;
     }
     public static void main(String[] args) {
